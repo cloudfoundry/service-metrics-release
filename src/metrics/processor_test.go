9111595eb9e8a3276e2bf3a936570c2aab66f032
Carson Long <lcarson@vmware.com>
2022-07-14 14:20:48 -0700

    Upgrade to ginkgo v2

--- src/metrics/processor_test.go
+++ src/metrics/processor_test.go
@@ -8,7 +8,7 @@ import (
 	"code.cloudfoundry.org/go-metric-registry/testhelpers"
 	"code.cloudfoundry.org/service-metrics/metrics"
 
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
